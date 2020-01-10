f3db1ece8a523f4e452923cc6499f4ad1b79d79b
surbhigoel191 <44471561+surbhigoel191@users.noreply.github.com>
2020-01-10 14:17:27 +0530

    Update handouts.py

--- handouts/handouts.py
+++ handouts/handouts.py
@@ -1,14 +1,18 @@
 import os
+import re
 from bs4 import BeautifulSoup
 import requests
+ 
 owd = os.getcwd()
 os.chdir("C:/Users/Surbhi/Desktop")
 soup = BeautifulSoup(open("AUGS-AGSR.html"), features="lxml")
-
-links = soup.find_all('a')
+val = input("Enter your course no.: ")
+links = soup.find_all('a') 
 for x in range(3, 312):
 	fullLink = links[x].get('href')
-	res = requests.get(fullLink).content
-	# print(fullLink)
-	with open( "myfile.pdf", "wb" ) as code :
-		code.write(res)
+	word= requests.get(fullLink).text
+	if (word.find(val) != -1):
+		res = requests.get(fullLink).content
+		with open( val+".pdf", "wb" ) as code :
+			code.write(res)
+		break
